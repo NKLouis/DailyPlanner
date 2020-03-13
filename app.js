cde47a28a2d32e367c7677f987f05b63be6142d1
naritchaya louis <naritchayalouis@gmail.com>
2020-03-12 22:00:23 -0500

    cleaned up

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 //Global variable
 var now = moment()
 
-
-
- //set up timeblocks for standard business hours
+//set up timeblocks for standard business hours
 $(document).ready(function () {
 
     hourArr = $('.hour').toArray()
     for (i = 0; i < hourArr.length; i++) {
 
-        $(hourArr[i]).siblings('textarea').text(localStorage.getItem($(hourArr[i]).attr('data-time')))
+     $(hourArr[i]).appendchild('textarea').text(localStorage.getItem($(hourArr[i]).attr('data-time')))
     }
 });
-
     for (i = 0; i < 9; i++) {
 
     
@@ -26,30 +23,35 @@ $(document).ready(function () {
 
     var textBlocks = $('<textarea>').css('width', '85%');
 
- // added a save icon
+    
+ // Color coded each block based on the current time
+    
+    if (now.isSame(moment('9:00 am', 'hh:mm A').add(i, 'hours'),)) {
+
+        $(textBlocks).addClass('present');
+    }
+     else if (now.isBefore(moment('9:00 am', 'hh:mm A').add(i, 'hours'),)) {
+
+        $(textBlocks).addClass('future');
+    } 
+     else if (now.isAfter(moment('9:00 am', 'hh:mm A').add(i, 'hours'),)) {
+
+        $(textBlocks).addClass('past');
+    }
+
+    // added a save icon
     var saveButton = $('<button>').addClass('saveBtn').html('<i class="fas fa-save"></i>');
 
     $(timeBlocks).after(textBlocks);
+
     $(textBlocks).after(saveButton);
 
 
- // Color coded each block based on the current time
-    
-    if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
-        $(textBlocks).addClass('present');
-    }
-     else if (now.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
-        $(textBlocks).addClass('future');
-    } 
-     else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
-        $(textBlocks).addClass('past');
-    }
-};
-
+}
 
 // Clicking a save icon will save the lastest content to localStorage.
 
  $('.saveBtn').on('click', function () {
 
-    localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())
+    localStorage.setItem($(this).appendchild('div.hour').attr('data-time'), $(this).siblings('textarea').val())
 });
